de968ef35644074b978ff3436474cdde6ef5f545
dalgim <dalgiewiczmateusz@gmail.com>
2017-04-01 16:12:59 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/dalgim/example/sb/cxf/CxfContractFirstSoapConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.cxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfContractFirstSoapConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxfContractFirstSoapConsumerApplication.class, args);
+	}
+}
