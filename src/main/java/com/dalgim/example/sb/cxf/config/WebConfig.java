4a9bf70964a41dea6e2fc5ac49c9e779cb1ad03c
dalgim <dalgiewiczmateusz@gmail.com>
2017-04-02 19:37:58 +0200

    Dodanie kontraktu wsdl w resource

---
+++ src/main/java/com/dalgim/example/sb/cxf/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.dalgim.example.sb.cxf.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by dalgim on 02.04.2017.
+ */
+@Configuration
+public class WebConfig {
+}
